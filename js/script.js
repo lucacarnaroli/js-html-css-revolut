828e9c0ad008563ed72d5430e1f219591f08806b
luca <luca.carnaroli98@gmail.com>
2020-01-20 21:33:47 +0100

    fine esecizio

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 $(document).ready(
   function() {
-    $('parent-dropdown').mouseenter(
+    $('.parent-dropdown').mouseenter(
       function () {
-        $(this).next('.dropdown').addClass('.active');
+        $(this).children('.dropdown').addClass('active');
+      }
+    );
+    $('.parent-dropdown').mouseleave(
+      function () {
+        $(this).children('.dropdown').removeClass('active');
+      }
+    );
+    $('.icon').mouseenter(
+      function () {
+        $('.arrow').hide();
+        $('.arrow-up').show().addClass('black');
       }
     )
-    // $('.conti').click(
-    //   function (){
-    //   $('.dropdown').hide();
-    // }
-  );
-}
+
+    $('.icon').mouseleave(
+      function () {
+      $('.arrow').show();
+      $('.arrow-up').hide().removeClass('black');
+    }
+  )
+
+  }
 );
