849a968c3a3b68feabfeae6b7f0c74c95f8efa7d
luca <luca.carnaroli98@gmail.com>
2020-01-20 17:21:52 +0100

    show/hide

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+$(document).ready(
+  function() {
+    $('.conti').mouseenter(
+      function () {
+        $('.dropdown').show();
+      }
+    )
+    $('.conti').mouseleave(
+      function (){
+      $('.dropdown').hide();
+    }
+  );
+}
+);
